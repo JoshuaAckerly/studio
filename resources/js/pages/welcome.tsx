d0098f26591165e0e8128c2137f7015439faf922
Joshua Ackerly <ackerlyj05@gmail.com>
2025-08-05 15:37:02 -0400

    Changes to various files

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,10 +1,13 @@
+import MainLayout from '@/layouts/main';
 import React from 'react';
 
 const Welcome: React.FC = () => (
-    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
-        <h1>Welcome to the Studio!</h1>
-        <p>This is your starting point. Begin building your application here.</p>
-    </div>
+    <MainLayout>
+        <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+            <h1>Welcome to the Studio!</h1>
+            <p>TComing Soon!</p>
+        </div>
+    </MainLayout>
 );
 
 export default Welcome;
